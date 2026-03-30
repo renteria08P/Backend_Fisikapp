583704ba2be90403b354e7b6752ea81e685d1c32
DanielaP <danielaparedes22@gmail.com>
2026-03-30 16:26:19 -0500

    feat: endpoint de informes con modelos, serializers y router

--- informes/views.py
+++ informes/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Informe
+from .serializers import InformeSerializer
 
 # Create your views here.
+class InformeViewSet(viewsets.ModelViewSet):
+    query = Informe.objects.all()
+    serializer_class = InformeSerializer
