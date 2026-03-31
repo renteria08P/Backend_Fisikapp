63f85eb38fa662f4b323e7869db9cef4c0dbbebe
DanielaP <danielaparedes22@gmail.com>
2026-03-31 16:25:40 -0500

    feat: endpoint de informes y resultados funcionando

--- informes/routers.py
+++ informes/routers.py
@@ -1,5 +1,9 @@
 from rest_framework.routers import DefaultRouter
 from .views import InformeViewSet
+from .views import ResultadoViewSet
 
 router = DefaultRouter()
 router.register(r'informes', InformeViewSet, basename = 'informes')
+router.register(r'resultados', ResultadoViewSet, basename = 'resultados')
+
+urlpatterns = router.urls
--- informes/views.py
+++ informes/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Informe
+from .models import Resultado
 from .serializers import InformeSerializer
+from .serializers import ResultadoSerializer
 
 # Create your views here.
 class InformeViewSet(viewsets.ModelViewSet):
-    query = Informe.objects.all()
+    queryset = Informe.objects.all()
     serializer_class = InformeSerializer
+
+class ResultadoViewSet(viewsets.ModelViewSet):
+    queryset = Resultado.objects.all()
+    serializer_class = ResultadoSerializer
