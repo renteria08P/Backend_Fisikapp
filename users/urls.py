e7e9b755847406ba5a0581f877609c47b32377a2
lilian1702 <163610265+lilian1702@users.noreply.github.com>
2026-04-07 00:11:22 -0500

    correccion de modelo y endpoints

--- users/urls.py
+++ users/urls.py
@@ -1,15 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UsersViewSet, registrar_usuario, recuperar_password, restablecer_password
-from .views import login_usuario
+from .views import UsersViewSet, recuperar_password, restablecer_password, login_usuario
 
 router = DefaultRouter()
 router.register(r'usuarios', UsersViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('registrar/', registrar_usuario),
+    path('login/', login_usuario),
     path('recuperar-password/', recuperar_password),
     path('restablecer-password/', restablecer_password),
-    path('login/', login_usuario),
 ]
