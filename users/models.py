6568b4f1c82362238386246e223a824f1a063b5f
EdwinMS91 <montenegrosalazaredwingiovany@gmail.com>
2026-04-11 15:14:09 -0500

    comi t e cambios

--- users/models.py
+++ users/models.py
@@ -25,6 +25,11 @@ class Users(AbstractBaseUser):
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     fecha_actualizacion = models.DateTimeField(auto_now=True)
 
+    # CAMPOS CLAVE
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
     USERNAME_FIELD = 'correo'
     REQUIRED_FIELDS = ['nombre']
 
@@ -32,3 +37,10 @@ class Users(AbstractBaseUser):
 
     def __str__(self):
         return self.nombre
+
+    # PERMISOS
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
