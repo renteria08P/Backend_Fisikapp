92f3616323b85fb8a099778cc767727fef922b63
EdwinMS91 <montenegrosalazaredwingiovany@gmail.com>
2026-04-13 20:53:09 -0500

    Merge

--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
-
 from .models import Users
 
 admin.site.register(Users)
 
 # Register your models here.
-
-from .models import Users
-
-admin.site.register(Users)
