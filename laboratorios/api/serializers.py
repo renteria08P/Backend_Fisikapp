aaeff5a1ff8ea3491c4676fb5aa85a9d3cd42add
cristian  Urrutia <urrutiacristian86@gmail.com>
2026-06-15 22:24:45 -0500

    commit para guardar datos sobre arreglo de logs y que aparesca el nombre de la persona que creo el laboratorio

--- laboratorios/api/serializers.py
+++ laboratorios/api/serializers.py
@@ -140,6 +140,10 @@ from laboratorios.models import PlantillaLaboratorio
 class PlantillaLaboratorioSerializer(
     serializers.ModelSerializer
 ):
+    creador_nombre = serializers.CharField(
+        source='creado_por.nombre',
+        read_only=True
+    )
 
     class Meta:
         model = PlantillaLaboratorio
