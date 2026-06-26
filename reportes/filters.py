b35ca2e60c41c053396d4e68a1cd17bdb1ce96e8
renteria08P <danielaparedes22@gmail.com>
2026-06-26 09:49:30 -0500

    Se agregan filtros por estudiante y laboratorio al historial de reportes

--- reportes/filters.py
+++ reportes/filters.py
@@ -1,4 +1,5 @@
 import django_filters
+from django.db.models import Q
 
 from .models import ReporteLaboratorio
 
@@ -6,11 +7,27 @@ from .models import ReporteLaboratorio
 class HistorialReporteFilter(django_filters.FilterSet):
 
     fecha = django_filters.DateFilter(
-        field_name='fecha_creacion'
+        field_name="fecha_creacion"
+    )
+
+    estudiante = django_filters.CharFilter(
+        method="filtrar_estudiante"
+    )
+
+    laboratorio = django_filters.CharFilter(
+        field_name="laboratorio__plantilla__titulo",
+        lookup_expr="icontains"
     )
 
     class Meta:
-
         model = ReporteLaboratorio
+        fields = [
+            "fecha",
+            "estudiante",
+            "laboratorio",
+        ]
 
-        fields = ['fecha']
+    def filtrar_estudiante(self, queryset, name, value):
+        return queryset.filter(
+            estudiantes__nombre__icontains=value
+        ).distinct()
